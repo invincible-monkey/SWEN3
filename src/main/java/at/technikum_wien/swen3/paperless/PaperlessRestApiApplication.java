a9b9a7f8eb2e8a624ce820771e64d5ea8fcd4e48
if22b091 <181574817+invincible-monkey@users.noreply.github.com>
2025-09-14 18:06:14 +0200

    Initialise Spring Boot project structure

---
+++ src/main/java/at/technikum_wien/swen3/paperless/PaperlessRestApiApplication.java
@@ -0,0 +1,13 @@
+package at.technikum_wien.swen3.paperless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperlessRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperlessRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/technikum_wien/swen3/paperless/PaperlessRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum_wien.swen3.paperless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperlessRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
